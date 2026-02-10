cd939f03483910c66c84ea6702c25fe977c7c0c6
Abhishek banyal <abhishekbanyal35@gmail.com>
2026-02-10 10:53:02 +0530

    react

---
+++ o1react/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Imp from './imp.jsx'
+function App() {
+
+  return (
+    <>
+   <Imp/>
+   <h3> react js</h3>
+   </>
+  )
+}
+
+export default App
---
+++ o1react/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+function app(){
+    <>
+    <h3>
+        hey today i am learning about reactjs</h3>
+        </>
+}
+
+createRoot(document.getElementById('root')).render(
+    app()
+    <App />
+)
